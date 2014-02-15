dca66bf65edece83846e67fd27cdc262ccce3162
cipher <gmgateway@gmail.com>
2014-02-15 10:43:18 +0900

    Init

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp')
+,	sass = require('gulp-ruby-sass')
+,	minifycss = require('gulp-minify-css')
+,	rename = require('gulp-rename');
+
+gulp.task('styles', function() {
+	gulp.src('scss/*.scss')
+	.pipe(sass({ style: 'expanded' }))
+	.pipe(gulp.dest('css'))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(minifycss())
+	.pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(['scss/*.scss'], ['styles']);
+});
+
+gulp.task('default', ['styles', 'watch']);
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Sass compile w/ gulpjs demo.
+
+Demo code for compile with gulp-ruby-sass.
+
+## Usage
+
+Install gulp plugins.
+
+	npm install
+
+Run gulp.
+
+	gulp
+
+## Changelog 0.0.1
+
+Initial.
