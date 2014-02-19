010546663caedd4d74dc96634eb6b29e4fc10b1f
cipher <gmgateway@gmail.com>
2014-02-19 10:02:59 +0900

    0.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp')
+,	gutil = require('gulp-util')
 ,	sass = require('gulp-ruby-sass')
 ,	minifycss = require('gulp-minify-css')
 ,	rename = require('gulp-rename');
 
 gulp.task('styles', function() {
-	gulp.src('scss/*.scss')
+	return gulp.src('scss/*.scss')
 	.pipe(sass({ style: 'expanded' }))
 	.pipe(gulp.dest('css'))
 	.pipe(rename({suffix: '.min'}))
